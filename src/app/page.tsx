17786700ebd265f77cb787638f374d6f0f3e6102
axelobscura <axosar@gmail.com>
2023-03-28 13:29:26 -0600

    entrada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Instituto Mexicano del Cemento y del Concreto A.C.
-        </p>
-        <div>
-          <a
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/logo-imcyc.svg"
-              alt="Webinars en concreto instituto mexicano del cemento y del concreto"
-              className={styles.vercelLogo}
-              width={200}
-              height={114}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/logo-imcyc.svg"
-          alt="Webinars en concreto instituto mexicano del cemento y del concreto"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Cemento <span>-&gt;</span>
-          </h2>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Certificaciones <span>-&gt;</span>
-          </h2>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Concreto <span>-&gt;</span>
-          </h2>
-        </a>
-      </div>
-    </main>
+    <div className="container-fluid login">
+    wrgfweewf  
+    </div>
   )
 }
