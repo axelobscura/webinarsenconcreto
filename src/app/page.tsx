9a74d455c52e19529bbfcb5fc60e619b048a5d65
axelobscura <axosar@gmail.com>
2023-03-28 14:01:16 -0600

    entrada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="container-fluid login">
-    wrgfweewf  
+      <div className='branding'>
+        <Image
+            src="/imcyc_registrada.svg"
+            alt="Webinars en concreto instituto mexicano del cemento y del concreto"
+            width="180"
+            height="100"
+        />
+        <h1>Plataforma Educativa</h1>
+        <p><small>Instituto Mexicano del Cemento y del Concreto A.C.</small></p>
+      </div>
     </div>
   )
 }
