039b316370c1279a62759e74ac8ffcdfc34a33ec
axel <axosar@gmail.com>
2023-05-04 20:10:32 -0600

    estilos

--- src/app/categorias/[nombre]/page.tsx
+++ src/app/categorias/[nombre]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'
+import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Nombre() {
 
@@ -9,10 +11,43 @@ export default function Nombre() {
   return (
     <div className="container-fluid login categorias">
       <div className='branding'>
-        <h2 className="text-white">{nombre}</h2>
+        <div className='row w-100 h-100 contenidos'>
+          <div className='col-3'>
+            <div className='barra_lateral'>
+              <div className='logo'>
+                <Image
+                  src="/imcyc_registrada.svg"
+                  alt="Webinars en concreto instituto mexicano del cemento y del concreto"
+                  width="150"
+                  height="70"
+                  style={{
+                    marginLeft: '-12px'
+                  }}
+                />
+              </div>
+              <ul className='menu'>
+                <li>
+                  <Link href=''>PRESENTACIÓN EJECUTIVA</Link>
+                </li>
+                <li>
+                  <Link href=''>PRESENTACIÓN GRABADA</Link>
+                </li>
+                <li>
+                  <Link href=''>EVALUACIÓN FINAL</Link>
+                </li>
+                <li>
+                  <Link href=''>CONTENIDO ADICIONAL</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='col'>
+            <h2 className="text-white">{nombre}</h2>
+          </div>
+        </div>
+        <p className='text-center p-0'><small>© 1959 - 2023 Instituto Mexicano del Cemento y del Concreto A.C.</small></p>
       </div>
       
-      <p className='text-center p-0'><small>© 1959 - 2023 Instituto Mexicano del Cemento y del Concreto A.C.</small></p>
     </div>
   )
 }
