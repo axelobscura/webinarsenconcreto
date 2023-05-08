2decbc8de4ec660d3bd35f8b0f1199c6f7a7f4cb
axel <axosar@gmail.com>
2023-05-08 14:04:53 -0600

    lib

--- src/app/components/Documento.tsx
+++ src/app/components/Documento.tsx
@@ -20,14 +20,14 @@ export default function Documento() {
           onReady={() => {
             ($("#containePDF") as any).flipBook({
               pages:[
-                  {src:"http://localhost:3000/images/book2/page1.jpg", thumb:"/images/book2/thumb1.jpg", title:"Cover"},
-                  {src:"http://localhost:3000/images/book2/page2.jpg", thumb:"/images/book2/thumb2.jpg", title:"Page two"},
-                  {src:"http://localhost:3000/images/book2/page3.jpg", thumb:"/images/book2/thumb3.jpg", title:"Page three"},
-                  {src:"http://localhost:3000/images/book2/page4.jpg", thumb:"/images/book2/thumb4.jpg", title:""},
-                  {src:"http://localhost:3000/images/book2/page5.jpg", thumb:"/images/book2/thumb5.jpg", title:"Page five"},
-                  {src:"http://localhost:3000/images/book2/page6.jpg", thumb:"/images/book2/thumb6.jpg", title:"Page six"},
-                  {src:"http://localhost:3000/images/book2/page7.jpg", thumb:"/images/book2/thumb7.jpg", title:"Page seven"},
-                  {src:"http://localhost:3000/images/book2/page8.jpg", thumb:"/images/book2/thumb8.jpg", title:"Last"}
+                  {src:"/images/book2/page1.jpg", thumb:"/images/book2/thumb1.jpg", title:"Cover"},
+                  {src:"/images/book2/page2.jpg", thumb:"/images/book2/thumb2.jpg", title:"Page two"},
+                  {src:"/images/book2/page3.jpg", thumb:"/images/book2/thumb3.jpg", title:"Page three"},
+                  {src:"/images/book2/page4.jpg", thumb:"/images/book2/thumb4.jpg", title:""},
+                  {src:"/images/book2/page5.jpg", thumb:"/images/book2/thumb5.jpg", title:"Page five"},
+                  {src:"/images/book2/page6.jpg", thumb:"/images/book2/thumb6.jpg", title:"Page six"},
+                  {src:"/images/book2/page7.jpg", thumb:"/images/book2/thumb7.jpg", title:"Page seven"},
+                  {src:"/images/book2/page8.jpg", thumb:"/images/book2/thumb8.jpg", title:"Last"}
               ],
               viewMode:"2d",
               layout:2
