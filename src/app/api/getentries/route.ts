b3c772f636b952f1492f55868db5c796167a4d02
axelobscura <axosar@gmail.com>
2023-05-10 10:34:18 -0600

    res

--- src/app/api/getentries/route.ts
+++ src/app/api/getentries/route.ts
@@ -6,8 +6,6 @@ console.log('hola');
 export async function GET(request: Request) {
   try {
     const results = await query('SELECT * FROM lideres');
-    //console.log(JSON.stringify(results));
-    //return JSON.stringify(results);
     return new Response(JSON.stringify(results));
   } catch ( error ) {
     console.log( error );
