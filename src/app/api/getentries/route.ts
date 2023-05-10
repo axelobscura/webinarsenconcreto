d76b7bfc9dc555ac67ed91e4546142c66b57bb74
axelobscura <axosar@gmail.com>
2023-05-10 10:06:39 -0600

    test-db

--- src/app/api/getentries/route.ts
+++ src/app/api/getentries/route.ts
@@ -1,7 +1,51 @@
 import { NextApiHandler } from 'next'
 import { query } from '../../../../lib/db'
 
+console.log('hola');
+
+export async function GET(request: Request) {
+  try {
+    const results = await query('SELECT * FROM lideres');
+    //console.log(JSON.stringify(results));
+    //return JSON.stringify(results);
+    return new Response(JSON.stringify(results));
+  } catch ( error ) {
+    console.log( error );
+  }
+  
+}
+
+/*
+export async function getUser() {
+  try {
+    const results = await query('SELECT * FROM lideres');
+    //console.log(JSON.stringify(results));
+    return JSON.stringify(results);
+  } catch ( error ) {
+    console.log( error );
+  }
+}
+
+getUser();
+*/
+
+/*
+export async function GET(request: Request) {
+  console.log('hola');
+  
+  try {
+    const results = await query(`SELECT * FROM lideres ORDER BY id DESC LIMIT 10`)
+    return JSON.stringify(results);
+  } catch (e: any) {
+    console.log(e);
+  }
+
+  return new Response('Hello, Next tester!')
+}
+*/
+/*
 export default async function handler(req: any, res: any) {
+  return res.json('hola');
   if (req.method === 'GET') {
     console.log('hola');
     try {
@@ -15,3 +59,4 @@ export default async function handler(req: any, res: any) {
     // Handle any other HTTP method
   }
 }
+*/
