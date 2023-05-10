688ac5b91a6c8a6af3bede35fa6779e24abee253
axel <axosar@gmail.com>
2023-05-09 18:07:42 -0600

    eval

--- src/app/components/Evaluacion.tsx
+++ src/app/components/Evaluacion.tsx
@@ -2,9 +2,6 @@
 import { evaluacion } from '../data/evaluacion.json'
 
 export default function Evaluacion({ categoria } : {categoria: string | null}) {
-
-  console.log(evaluacion);
-
   return (
       <div className='evaluacion'>
         <h2>{categoria}</h2>
@@ -12,15 +9,15 @@ export default function Evaluacion({ categoria } : {categoria: string | null}) {
           <div className='col-12'>
             <div className='preguntas'>
               {evaluacion.map(val => (
-                <div>
+                <div key={val.nombre}>
                   <p>TIEMPO RESTANTE: 25:30:48</p>
                   {val.preguntas.map((preg, i) => (
-                    <div className='bloque'>
+                    <div className='bloque' key={preg.pregunta}>
                       <h4>{i + 1} - {preg.pregunta}</h4>
                       <div>
                         <ul>
                         {preg.respuestas.map(val => (
-                          <li>
+                          <li key={val}>
                             <p><input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" /> {val}</p>
                           </li>
                         ))}
