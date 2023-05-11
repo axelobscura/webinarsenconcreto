da6d07caa1dc5f2608a8a91aae8e369d11ea0c49
axelobscura <axosar@gmail.com>
2023-05-11 09:31:34 -0600

    layout-examen

--- src/app/components/Evaluacion.tsx
+++ src/app/components/Evaluacion.tsx
@@ -24,7 +24,6 @@ export default function Evaluacion({ categoria } : {categoria: string | null}) {
 
   return (
       <div className='evaluacion'>
-        <h2>{categoria}</h2>
         <div className='row'>
           <div className='col-12'>
             <div className='preguntas'>
