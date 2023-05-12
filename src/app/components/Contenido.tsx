93268a16056e847d9db33a3267e31dd43d9b509d
axel <axosar@gmail.com>
2023-05-12 09:49:00 -0600

    libro

--- src/app/components/Contenido.tsx
+++ src/app/components/Contenido.tsx
@@ -25,7 +25,46 @@ export default function Contenido({ categoria } : {categoria: string | null}) {
           src="/js/flipbook.min.js" 
           onReady={() => {
             ($("#containePDF") as any).flipBook({
-              pdfUrl:"/libros/grado.pdf",
+              //pdfUrl:"/libros/grado.pdf",
+
+              pages: [
+                {
+                  'src': `https://webinarsenconcreto.com/images/2023/grado/pagina_001.jpg`,
+                  'thumb': `https://webinarsenconcreto.com/images/2023/grado/pagina_001.jpg`,
+                  'title': 'Página 1',
+                },
+                {
+                  'src': 'https://webinarsenconcreto.com/images/2023/grado/pagina_002.jpg',
+                  'thumb': 'https://webinarsenconcreto.com/images/2023/grado/pagina_002.jpg',
+                  'title': 'Página 2',
+                },
+                {
+                  'src': 'https://webinarsenconcreto.com/images/2023/grado/pagina_003.jpg',
+                  'thumb': 'https://webinarsenconcreto.com/images/2023/grado/pagina_003.jpg',
+                  'title': 'Página 3',
+                },
+                {
+                  'src': 'https://webinarsenconcreto.com/images/2023/grado/pagina_004.jpg',
+                  'thumb': 'https://webinarsenconcreto.com/images/2023/grado/pagina_004.jpg',
+                  'title': 'Página 4',
+                },
+                {
+                  'src': 'https://webinarsenconcreto.com/images/2023/grado/pagina_005.jpg',
+                  'thumb': 'https://webinarsenconcreto.com/images/2023/grado/pagina_005.jpg',
+                  'title': 'Página 6',
+                },
+                {
+                  'src': 'https://webinarsenconcreto.com/images/2023/grado/pagina_006.jpg',
+                  'thumb': 'https://webinarsenconcreto.com/images/2023/grado/pagina_006.jpg',
+                  'title': 'Página 7',
+                },
+                {
+                  'src': 'https://webinarsenconcreto.com/images/2023/grado/pagina_007.jpg',
+                  'thumb': 'https://webinarsenconcreto.com/images/2023/grado/pagina_007.jpg',
+                  'title': 'Página 8',
+                },
+              ],
+
               skin:"dark",
               singlePageMode:true,
               layout:4,
